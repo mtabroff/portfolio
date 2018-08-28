876382938267dc9bfb9c120364ff1d6f3fb3360b
Melanie Tabroff <mtabroff@gmail.com>
2018-08-28 17:40:28 -0400

    changes to hero and nav bar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
+
+
+
 function myFunction() {
    var x = document.getElementById("myTopnav");
    if (x.className === "links") {
