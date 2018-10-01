03f2ca0c7c22ef4733bdcdc43087d445cf81b45f
Melanie Tabroff <mtabroff@gmail.com>
2018-10-01 18:31:48 -0400

    added progress bar

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,9 @@
-$("#test p").addClass("load");​
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
